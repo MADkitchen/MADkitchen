10f29f9609eb71a1891048717e7a03af8ce9566d
Giovanni Cascione <ing.cascione@gmail.com>
2023-02-21 03:39:18 +0100

    improve lookup simple_query

--- include/class/Database/Lookup.php
+++ include/class/Database/Lookup.php
@@ -23,16 +23,16 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-/**
- * Collection of methods to handle MADkitchen lookup tables.
+/*
+ * Collection of methods to handle MADkitchen lookup tables anf lookup operation.
  *
  * @package     Database
- * @subpackage  Handler
+ * @subpackage  Lookup
  * @since       0.1
  */
-class Lookup {
 
-    /**
+class Lookup {
+    /*
      * Buffer to store in memory all lookup tables for quick access.
      *
      * Structure:
@@ -47,36 +47,56 @@ class Lookup {
      * @see is_lookup_table()
      * @since 0.1
      */
+
     public static $lookup_tables = array();
 
-    /**
+    /*
      * Checks if target table is defined as a lookup table
      *
      * @param string $class The target MADkitchen module class
      * @param string $table The target table name
      * @return bool True if table is defined as a lookup table, false otherwise
      */
+
     public static function is_lookup_table($class, $table) {
         return empty(\MADkitchen\Database\Handler::get_tables_data($class, $table)['lookup_table']) ? false : true;
     }
 
-    /**
-     * Check if query is simple one which can be addressed to lookup tables.
+    /*
+     * Checks if query is simple one which can be addressed to lookup tables.
      *
-     * Simple query contains only column search keys, which shall be all included in target table.
+     * Simple query contains only column search key (to resolve referred items) or one groupby key (to retrieve items list).
      *
      * @param string $class The target MADkitchen module class
      * @param string $table The target table name
      * @param string $query The BerlinDB Query class query array
      * @return bool True if is simple query.
      */
+
     public static function is_simple_query($class, $table, $query) {
-        return !empty($query) &&
-                !empty($table_cols = \MADkitchen\Database\Handler::get_tables_data($class, $table)['columns']) &&
-                count($item = array_intersect_assoc(array_keys($table_cols), array_keys($query))) === count($query);
+
+        $filtered_keys = [
+            'number',
+            'order'
+        ];
+
+        $query = array_diff_key($query, array_flip($filtered_keys));
+
+        if (empty($query) || empty($table_cols = \MADkitchen\Database\Handler::get_tables_data($class, $table)['columns']))
+            return false;
+
+        //simple item list query
+        if (key_exists('groupby', $query) && count($query) === 1)
+            return true;
+
+        //single item query
+        if (count($item = array_intersect_assoc(array_keys($table_cols), array_keys($query))) === count($query))
+            return true;
+
+        return false;
     }
 
-    /**
+    /*
      * Tries to retrieve target table from lookup table buffer.
      * If lookup table is not in the buffer yet, it is retrieved from database and added.
      *
@@ -84,15 +104,16 @@ class Lookup {
      * @param string $table The target table name
      * @return array|null Array of \Berlindb\Database\Row elements from the target table
      */
+
     public static function maybe_get_lookup_table($class, $table) {
         $retval = null;
         if (self::is_lookup_table($class, $table)) {
             if (!isset(self::$lookup_tables[$class][$table])) {
-                self::$lookup_tables[$class][$table] = \MADkitchen\Modules\Handler::$active_modules[$class]['class']->query($table, [
-                            'groupby' => [\MADkitchen\Database\Handler::get_primary_key_column_name($class, $table)],
-                            'order' => 'ASC',
-                            'number' => 0, //no limit to retrieved items
-                                ]
+                self::$lookup_tables[$class][$table] = \MADkitchen\Modules\Handler::$active_modules[$class]['class']->query($table,
+                                [
+                                    'groupby' => [\MADkitchen\Database\Handler::get_primary_key_column_name($class, $table)],
+                                ],
+                                true
                         )->items;
             }
             $retval = self::$lookup_tables[$class][$table];
@@ -113,13 +134,18 @@ class Lookup {
 
     public static function simple_lookup_query($class, $table, $query) {
         $retval = false;
+
         if (self::is_simple_query($class, $table, $query) && $table_data = self::maybe_get_lookup_table($class, $table)) {
+            if (key_exists('groupby', $query)) {
+                // Change groupby list to columns search arrays
+                $query = Handler::get_columns_array_from_rows($table_data, is_scalar($query['groupby']) ? [$query['groupby']] : $query['groupby'], true);
+            }
             foreach ($table_data as $row) {
                 foreach ($query as $column_name => $column_data) {
                     $include_row = false;
                     $column_data = is_array($column_data) ? $column_data : [$column_data];
                     foreach ($column_data as $column_item) {
-                        $include_row = $row->{$column_name} == $column_item ?: $include_row;
+                        $include_row = $row[$column_name] == $column_item ?: $include_row;
                     }
                 }
                 if (!empty($include_row))
